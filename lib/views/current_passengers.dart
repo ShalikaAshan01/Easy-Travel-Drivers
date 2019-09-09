961d59abbebdb486151d67276761272a5795be5d
Shalika <shalikaashan01@gmail.com>
2019-09-09 20:34:22 +0530

    app before adding firebase dependencies

---
+++ lib/views/current_passengers.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class CurrentPassengers extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _CurrentPassengersState();
+  }
+
+}
+
+class _CurrentPassengersState extends State<CurrentPassengers>{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
