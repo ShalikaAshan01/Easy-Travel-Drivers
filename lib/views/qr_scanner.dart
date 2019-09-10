fb3d8f7b42d58d6e10cb58e8f258a3bc6c7f2794
Shalika <shalikaashan01@gmail.com>
2019-09-10 12:53:41 +0530

    QR code validation and turn validation added

--- lib/views/qr_scanner.dart
+++ lib/views/qr_scanner.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:csse/views/home.dart';
+import 'package:csse/views/my_bottom_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -13,6 +16,9 @@ class QrScanner extends StatefulWidget{
 }
 
 class _QrScanner extends State<QrScanner>{
+  final String busRef = "ZLlJvSZM24uJqr2fXNn4";
+  bool isValid = false;
+  String text = "Scan Passengers QR Code";
 
   @override
   void initState() {
@@ -42,6 +48,7 @@ class _QrScanner extends State<QrScanner>{
         });
       }
     });
+    validateTurn();
   }
 
   @override
@@ -52,12 +59,22 @@ class _QrScanner extends State<QrScanner>{
         children: <Widget>[
           Container(
             height: 600.0,
-            child: QrCamera(
-              qrCodeCallback: (code) {
-                debugPrint(code);
-                _showAlert(code);
-              },
-            ),
+            child: isValid?Container(
+              child: QrCamera(
+                qrCodeCallback: (code){
+//                Navigator.pushReplacement(context,
+//                    MaterialPageRoute(builder: (context)=>MyBottomNavigationBar()));
+                  validateCode(code).then((bool val){
+//                    isValid = false;
+                    Navigator.pop(context);
+                    _showAlert(val);
+                  });
+                },
+              ),
+            ):
+            Container(
+            )
+            ,
           ),
           Center(
             child: Padding(
@@ -66,7 +83,7 @@ class _QrScanner extends State<QrScanner>{
                 child: Padding(
                   padding: EdgeInsets.all(15.0),
                   child: Text(
-                      "Scan Passengers QR Code",
+                      text,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 16.0,
@@ -81,13 +98,80 @@ class _QrScanner extends State<QrScanner>{
       ),
     );
   }
-  void _showAlert(String code){
+
+  Future<bool> validateCode(String code) async{
+    final DocumentSnapshot snapshot = await Firestore.instance
+        .collection("ride").document(code).get();
+    if (snapshot == null || !snapshot.exists) {
+      return false;
+    }else if(snapshot.data['status']=="completed"){
+      return false;
+    }
+    return true;
+  }
+
+  void _showAlert(bool res){
+    String title = "Great";
+    String content = "New Passenger was added";
+    if(!res){
+      title = "Whoops";
+      content = "Invalid token.Please recheck and read";
+    }
+    showMyDialog(title, content);
+  }
+  void showMyDialog(String title,String content){
     showDialog(
         context: context,
         builder: (context) => AlertDialog(
-          title: Text("QR code Detected"),
-          content: Text(code),
+          title: Text(title,style: TextStyle(fontWeight: FontWeight.bold),),
+          content: Text(content),
         )
     );
+  }
+
+  void validateTurn()async{
+
+     var snapshot = await Firestore.instance.collection("turn")
+         .where('bus',isEqualTo: busRef)
+//         .where('bus',isEqualTo: '/bus/$busRef')
+         .orderBy("start_time", descending: true)
+         .getDocuments();
+     List<DocumentSnapshot> docs = snapshot.documents;
+
+     DocumentSnapshot documentSnapshot = docs.first;
+     final now = DateTime.now();
+     final date = documentSnapshot.data['start_time'].toDate();
+     if(now.difference(date).inHours >24 || documentSnapshot.data["status"] == "completed"){
+//       Navigator.pop(context);
+     setState(() {
+       isValid = false;
+       text = "Please update turn information";
+     });
+//       showMyDialog("Invalid turn Information", "Please update your turn information");
+     Alert(
+       context: context,
+       type: AlertType.error,
+       title: "Invalid turn Information",
+       desc: "Please update your turn information.",
+       buttons: [
+         DialogButton(
+           child: Text(
+             "Ok",
+             style: TextStyle(color: Colors.white, fontSize: 20),
+           ),
+           onPressed: () => Navigator.pop(context),
+           width: 120,
+         )
+       ],
+     ).show();
+     }else{
+       setState(() {
+         isValid = true;
+       });
+     }
+  }
+
+  void addNewPassenger(String code){
+
   }
 }
