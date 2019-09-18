6e3fdbc4a26cbca3bc631c78d1b01d744ae6a3ff
ShalikaAshan01 <shalikaashan@outlook.com>
2019-09-18 09:00:47 +0530

    phone number verification added #issue

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,7 @@
+import 'package:csse/views/my_bottom_navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart' as prefix0;
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -10,6 +13,11 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   double imgSize = 150;
+  TextEditingController _controller = TextEditingController();
+  var _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String _verificationId = "";
+  String _phoneNumber;
 
   @override
   Widget build(BuildContext context) {
@@ -33,23 +41,35 @@ class _LoginState extends State<Login> {
                     Padding(
                       padding: const EdgeInsets.only(left: 12),
                       child: SizedBox(
-                        child: Text("+94",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 18.0),),
+                        child: Text(
+                          "+94",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 18.0),
+                        ),
                       ),
                     ),
                     Expanded(
                       child: Padding(
-                        padding: const EdgeInsets.fromLTRB(18,15,8,10),
+                        padding: const EdgeInsets.fromLTRB(18, 15, 8, 10),
                         child: Theme(
                           data: new ThemeData(
                             primaryColor: Colors.black,
                             primaryColorDark: Colors.black,
                           ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                labelText: "Phone Number".toUpperCase(),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5.0),
-                                )),
+                          child: Form(
+                            key: _formKey,
+                            child: TextFormField(
+                              validator: (String value) {
+                                return _phoneValidator(value);
+                              },
+                              keyboardType: TextInputType.number,
+                              controller: _controller,
+                              decoration: InputDecoration(
+                                  labelText: "Phone Number".toUpperCase(),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  )),
+                            ),
                           ),
                         ),
                       ),
@@ -69,7 +89,18 @@ class _LoginState extends State<Login> {
                     ),
                     color: Colors.black,
                     textColor: Colors.white,
-                    onPressed: () {},
+                    onPressed: () async{
+                      setState(() {
+                        if (_formKey.currentState.validate()) {
+                          debugPrint("..............press..................");
+                          _verifyPhoneNumber();
+//                         Navigator
+//                          .pushReplacement(context, MaterialPageRoute(
+//                           builder: (context)=> MyBottomNavigationBar()
+//                         ));
+                        }
+                      });
+                    },
                   ),
                 ),
               ),
@@ -82,4 +113,77 @@ class _LoginState extends State<Login> {
       ),
     );
   }
+
+  String _phoneValidator(String value) {
+    if (value.isEmpty) return "Phone Number cannot be empty";
+    if (value[0] == "0") value = value.substring(1);
+    value = "+94$value";
+    _phoneNumber = value;
+    Pattern pattern = '^(?:[+0]9)?[0-9]{10}\$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value)) return "Enter a valid Phone Number";
+    return null;
+  }
+
+  void _verifyPhoneNumber() async{
+
+    debugPrint("####################################################");
+    debugPrint("####################phone number $_phoneNumber################################");
+    debugPrint("####################################################");
+    final PhoneVerificationCompleted verificationCompleted =
+        (AuthCredential phoneCredential) {
+      _auth.signInWithCredential(phoneCredential);
+      debugPrint("************************************");
+      debugPrint("**************credientials**********************");
+      prefix0.debugPrint("recived phone auth credentials $phoneCredential");
+      debugPrint("************************************");
+      debugPrint("************************************");
+    };
+
+    final PhoneVerificationFailed phoneVerificationFailed =
+        (AuthException authException) {
+      debugPrint(
+          'Phone number verification failed. Code: ${authException.code}.'
+          ' Message: ${authException.message}');
+    };
+
+    final PhoneCodeSent codeSent =
+        (String verificationId, [int forceResendingToken]) async {
+      debugPrint('Please check your phone for the verification code.$verificationId');
+            _verificationId = verificationId;
+        };
+
+    final PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+        (String verificationId) {
+      _verificationId = verificationId;
+    };
+
+    await _auth.verifyPhoneNumber(
+        phoneNumber: _phoneNumber,
+        timeout: const Duration(seconds: 5),
+        verificationCompleted: verificationCompleted,
+        verificationFailed: phoneVerificationFailed,
+        codeSent: codeSent,
+        codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
+
+  }
+
+  void signInWithPhoneNumber()async{
+    final AuthCredential credential = PhoneAuthProvider.getCredential(
+      verificationId: _verificationId,
+//      smsCode: _smsController.text,
+    );
+    final FirebaseUser user =
+    (await _auth.signInWithCredential(credential)).user;
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState(() {
+    if (user != null) {
+//    _message = 'Successfully signed in, uid: ' + user.uid;
+    } else {
+//    _message = 'Sign in failed';
+    }
+    });
+  }
+
 }
